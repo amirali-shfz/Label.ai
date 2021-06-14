49f70a5aa9f3562f89cec90392ec8af579fa128b
Bilaal Hussain <Bilaalhussain1@gmail.com>
2021-06-14 18:49:45 -0400

    Update createTables.sql

--- queries/createTables.sql
+++ queries/createTables.sql
@@ -2,7 +2,7 @@
 -- label
 CREATE TABLE IF NOT EXISTS Label
 (
- lid  uuid NOT NULL,
+ lid  text NOT NULL,
  name text NOT NULL,
  CONSTRAINT PK_label PRIMARY KEY ( lid )
 );
@@ -13,14 +13,14 @@ CREATE TABLE IF NOT EXISTS Image
  iid          text NOT NULL,
  original_url text NOT NULL,
  small_url    text NOT NULL,
- rotation     int NOT NULL,
+ rotation     integer NOT NULL,
  CONSTRAINT PK_photo PRIMARY KEY ( iid )
 );
 
 -- user
 CREATE TABLE IF NOT EXISTS Member
 (
- mid      uuid NOT NULL,
+ mid      serial NOT NULL,
  username text NOT NULL,
  password text NOT NULL,
  trust    float NOT NULL,
@@ -31,15 +31,13 @@ CREATE TABLE IF NOT EXISTS Member
 -- classification
 CREATE TABLE IF NOT EXISTS Classification
 (
- cid            uuid NOT NULL,
- image_id       uuid NOT NULL,
- label          uuid NOT NULL,
+ cid            serial NOT NULL,
  confidence     float NOT NULL,
- true_count     int NOT NULL,
- false_count    int NOT NULL,
+ true_count     integer NOT NULL,
+ false_count    integer NOT NULL,
  pre_classified boolean NOT NULL,
  iid            text NOT NULL,
- lid            uuid NOT NULL,
+ lid            text NOT NULL,
  CONSTRAINT PK_classification PRIMARY KEY ( cid ),
  CONSTRAINT FK_39 FOREIGN KEY ( iid ) REFERENCES Image ( iid ),
  CONSTRAINT FK_45 FOREIGN KEY ( lid ) REFERENCES Label ( lid )
@@ -48,10 +46,10 @@ CREATE TABLE IF NOT EXISTS Classification
 -- submission
 CREATE TABLE IF NOT EXISTS Submission
 (
- sid           uuid NOT NULL,
+ sid           serial NOT NULL,
  correct_label boolean NOT NULL,
- mid           uuid NOT NULL,
- cid           uuid NOT NULL,
+ mid           integer NOT NULL,
+ cid           integer NOT NULL,
  CONSTRAINT PK_userclassification PRIMARY KEY ( sid ),
  CONSTRAINT FK_48 FOREIGN KEY ( mid ) REFERENCES Member ( mid ),
  CONSTRAINT FK_51 FOREIGN KEY ( cid ) REFERENCES Classification ( cid )
