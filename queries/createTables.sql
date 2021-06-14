b2cb3d76933cd8299e39fa1210951aa17b29461b
Bilaal Hussain <Bilaalhussain1@gmail.com>
2021-06-14 14:58:14 -0400

    Update createTables.sql

--- queries/createTables.sql
+++ queries/createTables.sql
@@ -1,58 +1,60 @@
 -- Create Tables Script
--- Label
-CREATE TABLE IF NOT EXISTS label
+-- label
+CREATE TABLE IF NOT EXISTS Label
 (
  lid  uuid NOT NULL,
- name text NOT NULL,
+ name string NOT NULL,
  CONSTRAINT PK_label PRIMARY KEY ( lid )
 );
 
 -- photo
 CREATE TABLE IF NOT EXISTS Photo
 (
- pid  uuid NOT NULL,
- "path" text NOT NULL,
+ pid          text NOT NULL,
+ original_url text NOT NULL,
+ small_url    text NOT NULL,
+ rotation     int NOT NULL,
  CONSTRAINT PK_photo PRIMARY KEY ( pid )
 );
 
 -- user
 CREATE TABLE IF NOT EXISTS "User"
 (
- uuid           uuid NOT NULL,
- username       text NOT NULL,
- password       text NOT NULL,
- trust          float NOT NULL,
- ProfilePicture text NOT NULL,
- CONSTRAINT PK_user PRIMARY KEY ( uuid )
+ uid      uuid NOT NULL,
+ username text NOT NULL,
+ password text NOT NULL,
+ trust    float NOT NULL,
+ name     text NOT NULL,
+ CONSTRAINT PK_user PRIMARY KEY ( uid )
 );
 
 -- classification
 CREATE TABLE IF NOT EXISTS Classification
 (
- cuid            uuid NOT NULL,
- photoId         uuid NOT NULL,
- label           uuid NOT NULL,
- confidence      float GENERATED ALWAYS AS (trueCount / (trueCount+falseCount)) STORED,
- trueCount       int NOT NULL,
- falseCount      int NOT NULL,
- isSourceOfTruth boolean NOT NULL,
- pid             uuid NOT NULL,
- lid             uuid NOT NULL,
- CONSTRAINT PK_classification PRIMARY KEY ( cuid ),
+ cid            uuid NOT NULL,
+ photo_id       uuid NOT NULL,
+ label          uuid NOT NULL,
+ confidence     float NOT NULL,
+ true_count     int NOT NULL,
+ false_count    int NOT NULL,
+ pre_classified boolean NOT NULL,
+ pid            text NOT NULL,
+ lid            uuid NOT NULL,
+ CONSTRAINT PK_classification PRIMARY KEY ( cid ),
  CONSTRAINT FK_39 FOREIGN KEY ( pid ) REFERENCES Photo ( pid ),
- CONSTRAINT FK_45 FOREIGN KEY ( lid ) REFERENCES label ( lid )
+ CONSTRAINT FK_45 FOREIGN KEY ( lid ) REFERENCES Label ( lid )
 );
 
--- userclassification
-CREATE TABLE IF NOT EXISTS UserClassification
+-- submission
+CREATE TABLE IF NOT EXISTS Submission
 (
- ucuid        uuid NOT NULL,
- correctLabel boolean NOT NULL,
- uuid         uuid NOT NULL,
- cuid         uuid NOT NULL,
- CONSTRAINT PK_userclassification PRIMARY KEY ( ucuid ),
- CONSTRAINT FK_48 FOREIGN KEY ( uuid ) REFERENCES "User" ( uuid ),
- CONSTRAINT FK_51 FOREIGN KEY ( cuid ) REFERENCES Classification ( cuid )
+ sid           uuid NOT NULL,
+ correct_label boolean NOT NULL,
+ uid           uuid NOT NULL,
+ cid           uuid NOT NULL,
+ CONSTRAINT PK_userclassification PRIMARY KEY ( sid ),
+ CONSTRAINT FK_48 FOREIGN KEY ( uid ) REFERENCES "User" ( uid ),
+ CONSTRAINT FK_51 FOREIGN KEY ( cid ) REFERENCES Classification ( cid )
 );
 
 -- index
@@ -66,12 +68,12 @@ CREATE INDEX fkIdx_46 ON Classification
  lid
 );
 
-CREATE INDEX fkIdx_49 ON UserClassification
+CREATE INDEX fkIdx_49 ON Submission
 (
- uuid
+ uid
 );
 
-CREATE INDEX fkIdx_52 ON UserClassification
+CREATE INDEX fkIdx_52 ON Submission
 (
- cuid
+ cid
 );
