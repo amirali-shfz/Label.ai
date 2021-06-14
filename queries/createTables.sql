3a727e94173ac0cf65bd5112e8bcbfb199635aa6
Bilaal Hussain <Bilaalhussain1@gmail.com>
2021-06-14 15:48:30 -0400

    Update createTables.sql

--- queries/createTables.sql
+++ queries/createTables.sql
@@ -7,41 +7,41 @@ CREATE TABLE IF NOT EXISTS Label
  CONSTRAINT PK_label PRIMARY KEY ( lid )
 );
 
--- photo
-CREATE TABLE IF NOT EXISTS Photo
+-- image
+CREATE TABLE IF NOT EXISTS Image
 (
- pid          text NOT NULL,
+ iid          text NOT NULL,
  original_url text NOT NULL,
  small_url    text NOT NULL,
  rotation     int NOT NULL,
- CONSTRAINT PK_photo PRIMARY KEY ( pid )
+ CONSTRAINT PK_photo PRIMARY KEY ( iid )
 );
 
 -- user
-CREATE TABLE IF NOT EXISTS "User"
+CREATE TABLE IF NOT EXISTS Member
 (
- uid      uuid NOT NULL,
+ mid      uuid NOT NULL,
  username text NOT NULL,
  password text NOT NULL,
  trust    float NOT NULL,
  name     text NOT NULL,
- CONSTRAINT PK_user PRIMARY KEY ( uid )
+ CONSTRAINT PK_user PRIMARY KEY ( iid )
 );
 
 -- classification
 CREATE TABLE IF NOT EXISTS Classification
 (
  cid            uuid NOT NULL,
- photo_id       uuid NOT NULL,
+ image_id       uuid NOT NULL,
  label          uuid NOT NULL,
  confidence     float NOT NULL,
  true_count     int NOT NULL,
  false_count    int NOT NULL,
  pre_classified boolean NOT NULL,
- pid            text NOT NULL,
+ iid            text NOT NULL,
  lid            uuid NOT NULL,
  CONSTRAINT PK_classification PRIMARY KEY ( cid ),
- CONSTRAINT FK_39 FOREIGN KEY ( pid ) REFERENCES Photo ( pid ),
+ CONSTRAINT FK_39 FOREIGN KEY ( iid ) REFERENCES Image ( iid ),
  CONSTRAINT FK_45 FOREIGN KEY ( lid ) REFERENCES Label ( lid )
 );
 
@@ -50,17 +50,17 @@ CREATE TABLE IF NOT EXISTS Submission
 (
  sid           uuid NOT NULL,
  correct_label boolean NOT NULL,
- uid           uuid NOT NULL,
+ mid           uuid NOT NULL,
  cid           uuid NOT NULL,
  CONSTRAINT PK_userclassification PRIMARY KEY ( sid ),
- CONSTRAINT FK_48 FOREIGN KEY ( uid ) REFERENCES "User" ( uid ),
+ CONSTRAINT FK_48 FOREIGN KEY ( mid ) REFERENCES Member ( mid ),
  CONSTRAINT FK_51 FOREIGN KEY ( cid ) REFERENCES Classification ( cid )
 );
 
 -- index
 CREATE INDEX fkIdx_40 ON Classification
 (
- pid
+ iid
 );
 
 CREATE INDEX fkIdx_46 ON Classification
@@ -70,7 +70,7 @@ CREATE INDEX fkIdx_46 ON Classification
 
 CREATE INDEX fkIdx_49 ON Submission
 (
- uid
+ mid
 );
 
 CREATE INDEX fkIdx_52 ON Submission
