31d4205c2427e389c17b062e32310a5dc08dcaaf
Walker Hildebrand <walker99113@gmail.com>
2021-07-04 15:48:49 -0400

    remove unused labels

--- queries/populate_all.sql
+++ queries/populate_all.sql
@@ -1,3 +1,4 @@
 \copy label FROM './dataset_processing/csv/db_ready/label.csv' WITH (FORMAT csv, HEADER);
 \copy image FROM './dataset_processing/csv/db_ready/image.csv' WITH (FORMAT csv, HEADER);
 \copy classification(img_id, label_id, true_count, false_count, pre_classified) FROM './dataset_processing/csv/db_ready/classification.csv' WITH (FORMAT csv, HEADER);
+DELETE FROM label WHERE label_id not in (SELECT DISTINCT label_id FROM classification);
