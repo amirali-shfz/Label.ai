b269056e8a87e30c9319866a320894ff744d01a6
Walker Hildebrand <walker99113@gmail.com>
2021-07-05 12:47:56 -0400

    Create ClassificationView and add more false data

--- queries/create_all.sql
+++ queries/create_all.sql
@@ -58,4 +58,38 @@ CREATE INDEX ON Submission ( member_id );
 CREATE INDEX ON Submission ( class_id );
 
 -- views
-CREATE OR REPLACE VIEW img_label (img_id, url, label) AS SELECT original_url, NAME FROM classification NATURAL JOIN label NATURAL JOIN image;
+CREATE OR REPLACE VIEW ImgLabel (img_id, url, label) AS
+    SELECT img_id, original_url, name FROM classification NATURAL JOIN label NATURAL JOIN image;
+
+
+CREATE OR REPLACE VIEW ClassificationView (
+    class_id,
+    img_id,
+    label_id,
+    pre_classified,
+    confidence
+) AS
+SELECT
+    class_id,
+    img_id,
+    label_id,
+    pre_classified,
+    CASE WHEN total_votes = 0 THEN 0 ELSE ((positive_votes + 1.9208) / total_votes - 1.96 * SQRT(CAST ((positive_votes * (total_votes-positive_votes)) / total_votes + 0.9604 AS FLOAT)) / (total_votes)) / (1 + 3.8416 / (total_votes)) END
+FROM (
+    SELECT
+        class_id,
+        img_id,
+        label_id,
+        pre_classified,
+        COALESCE(positive_votes, 5) positive_votes,
+        COALESCE(total_votes, 10) total_votes
+    FROM
+        classification NATURAL LEFT JOIN (
+            SELECT
+                class_id,
+                5+SUM(CASE WHEN correct_label THEN trust ELSE 0 END) AS positive_votes,
+                10+SUM(trust) as total_votes
+            FROM
+                submission NATURAL JOIN member GROUP BY class_id ORDER BY class_id
+        ) as foo
+    ) as classification_votes;
--- queries/populate_all.sql
+++ queries/populate_all.sql
@@ -1,4 +1,14 @@
-\copy label FROM '../dataset_processing/csv/db_ready/label.csv' WITH (FORMAT csv, HEADER);
-\copy image FROM '../dataset_processing/csv/db_ready/image.csv' WITH (FORMAT csv, HEADER);
-\copy classification(img_id, label_id, true_count, false_count, pre_classified) FROM '../dataset_processing/csv/db_ready/classification.csv' WITH (FORMAT csv, HEADER);
+\copy label FROM './dataset_processing/csv/db_ready/label.csv' WITH (FORMAT csv, HEADER);
+\copy image FROM './dataset_processing/csv/db_ready/image.csv' WITH (FORMAT csv, HEADER);
+\copy classification(img_id, label_id, true_count, false_count, pre_classified) FROM './dataset_processing/csv/db_ready/classification.csv' WITH (FORMAT csv, HEADER);
 DELETE FROM label WHERE label_id not in (SELECT DISTINCT label_id FROM classification);
+
+
+INSERT INTO member(username, password, trust, name) VALUES
+    ('wbhildeb', '1092745', 0.5, 'Walker'),
+    ('kfeng', '1289501', 0.75, 'Kevin'),
+    ('super_trust', '2145312', 10, 'God'),
+    ('no_trust', '12380', 0, 'TrollMan42069');
+
+INSERT INTO submission(class_id, member_id, correct_label) VALUES
+    (3, 1, False), (2, 2, False), (12, 1, False), (14, 2, False), (9, 3, True), (10, 2, True), (1, 3, True), (3, 1, True), (1, 1, False), (7, 3, False), (14, 2, False), (11, 1, True), (5, 2, True), (7, 2, True), (4, 2, True), (6, 3, True), (14, 3, True), (13, 2, True), (9, 3, True), (11, 3, False), (12, 1, False), (11, 1, False), (8, 3, True), (8, 2, True), (13, 2, True), (8, 2, False), (7, 1, True), (13, 3, False), (10, 3, False), (4, 2, False);
