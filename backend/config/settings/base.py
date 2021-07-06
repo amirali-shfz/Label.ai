a0b2fc7baaa4e87effbb327c46d7d2219350978e
Amirali <aasharifzad2@gmail.com>
2021-07-05 21:02:24 -0400

    minor

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -58,7 +58,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 # ------------------------------------------------------------------------------
 CORS_ORIGIN_ALLOW_ALL = True 
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3030/',
+]
 
 # APPS
 # ------------------------------------------------------------------------------
